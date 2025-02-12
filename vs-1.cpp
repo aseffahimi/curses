460cbb3cc1708620f28eb7a2bd85e54eb1083fc9
User <asef.fahimi@gmail.com>
2025-02-12 15:00:46 +0330

    ncurses1

---
+++ vs-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+int age;
+cout << "enter your age:" ;
+int i=25*25;
+cin >> age;
+cout << "age entered:" << age ;
+cin >> i  ;
+
+cout << "by";
+
+}
